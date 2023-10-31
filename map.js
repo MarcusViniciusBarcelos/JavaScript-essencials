9ac2d1f5bb97decd0b975599ff8ba816b2324a43
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-31 14:11:50 -0300

    exemplos de map com objetos

--- map.js
+++ map.js
@@ -29,4 +29,28 @@ const novoArray = arrayOriginal.map(function(elemento, índice, array) {
 const numeros = [1, 2, 3, 4, 5];
 const aoQuadrado = numeros.map(num => num ** 2);
 // Resultado: [1, 4, 9, 16, 25]
+// O método map() é muito útil para transformar os elementos de um array em outros elementos, como strings, objetos, outros arrays, etc. 
 
+const pessoas = [
+  { nome: 'Maria', idade: 23 },
+  { nome: 'João', idade: 18 },
+  { nome: 'José', idade: 30 },
+  { nome: 'Paulo', idade: 16 },
+];
+
+const nomes = pessoas.map(obj => obj.nome); // retornando uma string apenas com o nome da pessoa
+// Resultado: ['Maria', 'João', 'José', 'Paulo']
+const idades = pessoas.map(obj => ({ idade: obj.idade })); // retornando um objeto com a idade da pessoa
+// Resultado: [{ idade: 23 }, { idade: 18 }, { idade: 30 }, { idade: 16 }]
+const comIds = pessoas.map((obj, index) => {
+  const newObj = { ...obj };
+  newObj.id = index + 1;
+  return newObj;
+}); // retornando um objeto com o nome, idade e id da pessoa
+
+// Resultado: [
+//   { nome: 'Maria', idade: 23, id: 1 },
+//   { nome: 'João', idade: 18, id: 2 },
+//   { nome: 'José', idade: 30, id: 3 },
+//   { nome: 'Paulo', idade: 16, id: 4 },
+// ]
